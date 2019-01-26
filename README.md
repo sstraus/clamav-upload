2cbcab5a2a2b47f2a334b37ccaadd1f9d8df2d30
Stefano <stefano@straus.it>
2019-01-26 22:36:36 +0100

    Add support for retrieve, security, many fixes and README

--- README.md
+++ README.md
@@ -1,14 +1,64 @@
-# ClamAv Upload
+# ClamAV Upload
 
-Antivirus docker
+Antivirus docker with the latest ClamAV exposing an HTTP endpoint
 
-/virus/version
+###### [GET] /virus/version
 
-/virus/ping
+Return the version of the ClamAV engine
 
-/virus/upload/multipart
+{
+    "result": "ok",
+    "version": "ClamAV 0.101.1/25332/Sat Jan 26 10:28:25 2019"
+}
 
-/virus/upload/binary
+###### [GET] /virus/ping
 
-**work in progess**
+Return the status of the ClamAV engine
 
+{
+    "result": "ok",
+    "status": "alive"
+}
+
+###### [POST] /virus/upload/binary
+
+Accept a binary uploaded file and return if the file is infected.
+
+{
+    "result": "ok",
+    "infected": false
+}
+
+or
+
+{
+    "result": "ok",
+    "infected": true,
+    "description": "Eicar-Test-Signature"
+
+}
+
+Default response is in JSON format, but you can change to XML setting the <i>accept</i> header to *application/xml*
+
+In case of error, the result is "fail" and error contains the error description
+
+{
+    "result": "fail",
+    "error": "Malformed Response[UNKNOWN COMMAND]"
+}
+
+###### [POST] /virus/upload/multipart
+
+Accept a multipart uploaded file with name "file" and return if the file is infected.
+
+###### [GET] /virus/get?url=http://www.files.com/somefile.exe
+
+Will download the file and scan the binary data against viruses.
+
+This docker is not build to be publicly exposed to the INTERNET but to offer more protection, you can run the docker with a KEY variable set and provide a header with the same key in the request to authenticate requests.
+
+##### Docker usage:
+
+docker run -p 80:3000 clamav-upload -e KEY="xxxxx"
+
+For any question drop a line to stefano+docker [AT] straus [DOT] it
--- app/application.ts
+++ app/application.ts
@@ -1,15 +1,20 @@
 import express from "express";
-
+import http = require("http");
+import https = require("https");
 import multer = require("multer");
-import getRawBody from "raw-body";
+import easyxml from "easyxml";
 import * as stream from "stream";
 import bodyParser = require("body-parser");
 import clamav from "clamav.js";
-import { RequestHandler } from "express-serve-static-core";
-import { read } from "fs";
 
 var upload = multer({
   storage: multer.memoryStorage()
+});
+var serializer = new easyxml({
+  singularize: true,
+  rootElement: 'response',
+  dateFormat: 'ISO',
+  manifest: true
 });
 
 export class Application {
@@ -21,7 +26,73 @@ export class Application {
 
   start() {
     console.log("Application Started");
-    this.app.disable('x-powered-by');
+    console.log('Key:' + process.env.KEY);
+    this.app.set('x-powered-by', false);
+    this.app.set('etag', false);
+
+    this.app.use(function (req, res, next) {
+      (<any>res).sendData = function (statusCode: number, obj: any) {
+        if (req.accepts('json') || req.accepts('text/html')) {
+          res.header('Content-Type', 'application/json');
+          res.status(statusCode).send(obj);
+        } else if (req.accepts('xml')) {
+          res.header('Content-Type', 'text/xml');
+          var xml = serializer.render(obj, "");
+          res.status(statusCode).send(xml);
+        } else {
+          res.send(406);
+        }
+      };
+      next();
+    });
+
+    this.app.use(function (req, res, next) {
+      if (process.env.KEY && req.header("key") != process.env.KEY) {
+        next("Access Denied");
+      } else
+        next();
+    });
+
+    this.app.use(function (req, res, next) {
+      (<any>res).scan = function (readStream) {
+        clamav
+          .createScanner(3310, "127.0.0.1")
+          .scan(readStream, function (err, object, malicious) {
+            if (err) {
+              console.log(err);
+              next(err);
+            } else if (malicious) {
+              console.log(malicious + " FOUND");
+              (<any>res).sendData(200, { result: "ok", "infected": true, "description": malicious });
+            } else {
+              console.log("OK");
+              (<any>res).sendData(200, { result: "ok", "infected": false });
+            }
+          });
+      }
+      next();
+    });
+
+    this.app.get("/virus/about", function (req, res, next) {
+      console.log('About');
+      res.send((<any>res).sendData(200, { result: "ok", "about": "stefano+docker@straus.it" }));
+    });
+
+    this.app.get("/virus/get", function (req, res, next) {
+    var client =  (req.query.url.toString().indexOf("https") === 0) ? https : http;
+        client.get(req.query.url, function (response) {
+        const readStream = new stream.Readable();
+        response.on('data', function (chunk) {
+          readStream.push(chunk);
+        }).on('end', function () {
+          readStream.push(null);
+          (<any>res).scan(readStream);
+        });
+      }).on('error', function (err) { // Handle errors
+        next(err);
+      });
+    });
+
     this.app.get("/virus/version", function (req, res, next) {
       clamav.version(3310, '127.0.0.1', 1000, function (err, version) {
         if (err) {
@@ -30,10 +101,11 @@ export class Application {
         }
         else {
           console.log('Version is [' + version + ']');
-          res.send('Version is [' + version + ']');
+          res.send((<any>res).sendData(200, { result: "ok", "version": version }));
         }
       })
     });
+
     this.app.get("/virus/ping", function (req, res, next) {
       clamav.ping(3310, '127.0.0.1', 1000, function (err) {
         if (err) {
@@ -42,7 +114,7 @@ export class Application {
         }
         else {
           console.log('ClamAV is alive');
-          res.send('ClamAV is alive');
+          res.send((<any>res).sendData(200, { result: "ok", "status": "alive" }));
         }
       })
     });
@@ -55,20 +127,7 @@ export class Application {
       const readStream = new stream.Readable();
       readStream.push(req.file.buffer);
       readStream.push(null);
-      clamav
-        .createScanner(3310, "127.0.0.1")
-        .scan(readStream, function (err, object, malicious) {
-          if (err) {
-            console.log(object.path + ": " + err);
-            next(err);
-          } else if (malicious) {
-            console.log(object.path + ": " + malicious + " FOUND");
-            next(new Error(malicious + " FOUND"));
-          } else {
-            console.log(object.path + ": OK");
-            res.send("OK");
-          }
-        });
+      this.scan(readStream, res, next);
     });
 
     this.app.use(bodyParser.raw({ type: 'application/octet-stream', limit: '25mb' }))
@@ -80,26 +139,13 @@ export class Application {
       const readStream = new stream.Readable();
       readStream.push(req.body);
       readStream.push(null);
-      clamav
-        .createScanner(3310, "127.0.0.1")
-        .scan(readStream, function (err, object, malicious) {
-          if (err) {
-            console.log(object.path + ": " + err);
-            next(err);
-          } else if (malicious) {
-            console.log(object.path + ": " + malicious + " FOUND");
-            next(new Error("Virus Detected"));
-          } else {
-            console.log(object.path + ": OK");
-            res.send("OK");
-          }
-        });
+      (<any>res).scan(readStream);
     });
 
     this.app.use(function (err, req, res, next) {
       if (err !== null) {
         console.log(err);
-        res.send({ result: "fail", error: err.message });
+        (<any>res).sendData(500, { result: "fail", error: err.message });
       } else {
         next();
       }
